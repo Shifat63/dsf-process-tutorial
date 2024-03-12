93ce19fc05b1739f200a31b83bca4336b5943804
Md Emtazul Haque <shifat63csedu@gmail.com>
2024-03-12 16:25:26 +0100

    Test

--- tutorial-process/src/main/java/dev/dsf/process/tutorial/service/DicTask.java
+++ tutorial-process/src/main/java/dev/dsf/process/tutorial/service/DicTask.java
@@ -1,11 +1,13 @@
 package dev.dsf.process.tutorial.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 
 import dev.dsf.bpe.v1.ProcessPluginApi;
 import dev.dsf.bpe.v1.activity.AbstractServiceDelegate;
 import dev.dsf.bpe.v1.variables.Variables;
 
+@Slf4j
 public class DicTask extends AbstractServiceDelegate
 {
 	public DicTask(ProcessPluginApi api)
@@ -16,6 +18,7 @@ public class DicTask extends AbstractServiceDelegate
 	@Override
 	protected void doExecute(DelegateExecution execution, Variables variables)
 	{
-
+		log.info("Hello. Recepient_System: "+ variables.getStartTask().getRestriction().getRecipientFirstRep().getIdentifier().getSystem()
+		+" Recepient_Value:"+ variables.getStartTask().getRestriction().getRecipientFirstRep().getIdentifier().getValue());
 	}
 }
